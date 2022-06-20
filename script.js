79a7a43fea129a2a00800b99e3c8cb7946090dc6
Ahkal <bp@sojourn.llc>
2022-06-20 08:18:24 -0400

    Null Coalescing

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // -----Arrow functions-----
-const arr = [1,2,3,4,5];
-const arr2 = [6,7,8,9,10,11,12];
-const person = {
-    company: 'President',
-    userName: 'AK',
-    description: ['Sojourner', 'Foodie', 'Developer', 'Lover', 'Minimalist @ heart..'],
-    favNum: 12
-}
+// const arr = [1,2,3,4,5];
+// const arr2 = [6,7,8,9,10,11,12];
+// const person = {
+//     company: 'President',
+//     userName: 'AK',
+//     description: ['Sojourner', 'Foodie', 'Developer', 'Lover', 'Minimalist @ heart..'],
+//     favNum: 12
+// }
 // OLD
 // const doubled = arr.map(double);
 // function double(num){
@@ -16,8 +16,8 @@ const person = {
 // NEW
 // With one parameter, no need for () around num.
 // implicitly returns num*2;
-const doubled = arr.map(num => num*2);
-console.log(doubled);
+// const doubled = arr.map(num => num*2);
+// console.log(doubled);
 
 // ----- Destructuring Assignment | ARRAY -----
 // OLD
@@ -40,10 +40,10 @@ console.log(doubled);
 
 // NEW
 // hungry doesn't exist above, b/c of that, hungry will be created in the object and have a default val of true
-const {company, userName: nickname, hungry=true, ...rest} = person;
-console.log(company, nickname);
-console.log(hungry); // Default value.
-console.log(rest );
+// const {company, userName: nickname, hungry=true, ...rest} = person;
+// console.log(company, nickname);
+// console.log(hungry); // Default value.
+// console.log(rest );
 
 // OLD
 // function printName(person){
@@ -52,31 +52,42 @@ console.log(rest );
 
 // NEW
 // Use {} to select on the necessery values 
-function printName({userName}){
-    console.log(userName);
-}
+// function printName({userName}){
+//     console.log(userName);
+// }
 // Pass in object
-printName(person);
+// printName(person);
 
 // ----- Spread Operator -----
 
 // because this is only 2 variables, the rest of the array that's passed in is dropped.
-function add(x,y) {
-    console.log(x+y);
-}
+// function add(x,y) {
+//     console.log(x+y);
+// }
+
 // OLD
 // add(arr[0],arr[1]);
 
 // NEW
-add(...arr); //3
+// add(...arr); //3
 
-const combined = [0, ...arr, ...arr2];
-console.log(combined);
+// const combined = [0, ...arr, ...arr2];
+// console.log(combined);
 
 // The rest of the values passed in, stored inside rest array variable.
-function logParams(x, ...rest){
-    console.log(x);
-    console.log(rest);
-}
+// function logParams(x, ...rest){
+//     console.log(x);
+//     console.log(rest);
+// }
 
-logParams(1,234,567,89); //look at function
+// logParams(1,234,567,89); //look at function
+
+// ----- Null Coalescing -----
+const drink = 'Olipop';
+
+// OLD
+// const defaultDrink = drink != null ? drink : 'Water';
+
+// NEW
+const defaultDrink = drink ?? 'Water';
+console.log(`I'd like some ${defaultDrink} to drink please.`);
